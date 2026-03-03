e11ed6b16b8e58c153c0ee3032b86e0386e6f3c1
STGMS <101299771+STGMS@users.noreply.github.com>
2026-03-03 10:32:31 +0000

    Add files via upload

---
+++ Basics/HexaWrite.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX 1000000
+
+int main() {
+    char text[MAX];
+
+    printf("Input your text:\n");
+
+    if (fgets(text, sizeof(text), stdin)) {
+        printf("Text: %s\nHex:  ", text);
+
+        for (int i = 0; text[i] != '\0'; i++) {
+            printf("%02X ", (unsigned char)text[i]);
+            //if ((i + 1) % 16 == 0) printf("\n      "); // Salto cada 16 bytes
+        }
+    }
+
+    printf("\n\nPresiona Enter para salir...");
+    getchar();
+
+    return 0;
+}
